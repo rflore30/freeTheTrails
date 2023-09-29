a188ac491711364668138c322378b44830db71f8
Ryan <r_j_flores@yahoo.com>
2023-09-28 20:00:07 -0700

    Created a basic Trail class

--- src/freethetrails/utilities.py
+++ src/freethetrails/utilities.py
@@ -1,11 +1,14 @@
 
 class Trail:
-    def __init__(self, name, length, rating):
+    def __init__(self, name: str, length: int, rating):
         self.name = name
         self.length = length
         self.rating = rating
 
+    def __str__(self):
+        return f"{self.name} is {self.length} miles long and has a {self.rating} star rating"
 
-def parse_trails(page: str) -> list{trails}:
+
+def parse_trails(page: str) -> list[Trail]:
     """
     Given a page of info from AllTrails, return a list of Trails"""
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,5 +1,27 @@
-import freethetrails.utilities
+import freethetrails.utilities as utl
 
 
-def test_trail_class():
-    assert False
+def test_trail_init():
+    """Test to see that Trail is initialized properly"""
+    attributes = (
+        ('Trail1', 5.1, 4.3),
+        ('Trail2', 6.7, 3.1)
+    )
+
+    for attribute in attributes:
+        actual = utl.Trail(*attribute)
+        assert actual.name == attribute[0]
+        assert actual.length == attribute[1]
+        assert actual.rating == attribute[2]
+
+
+def test_trail_str():
+    """Test to see that str(Trail) has all needed info"""
+    length = 5
+    rating = 2
+    name = 'my_trail'
+    my_trail = utl.Trail(length, rating, name)
+
+    assert str(length) in str(my_trail)
+    assert str(rating) in str(my_trail)
+    assert name in str(my_trail)
